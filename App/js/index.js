ad201d4fce679fec0cacaf931b9dcb6a8df302d2
Bluppie06 <51677733+Bluppie06@users.noreply.github.com>
2019-06-10 21:31:41 +0200

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -56,8 +56,10 @@ xui.Class('App', 'xui.Module',{
                         "desc":"Action 1",
                         "type":"page",
                         "target":"App.Writer",
-                        "args":[true],
-                        "method":"switch",
+                        "args":[
+                            { }
+                        ],
+                        "method":"open",
                         "event":1
                     },
                     "_xui_ui_button8_onclick"
@@ -67,80 +69,6 @@ xui.Class('App', 'xui.Module',{
                         "font-size":"20px"
                     }
                 })
-            );
-            
-            append(
-                xui.create("xui.UI.MenuBar")
-                .setHost(host,"xui_ui_menubar2")
-                .setItems([
-                    {
-                        "id":"menu1",
-                        "sub":[
-                            {
-                                "id":"normal",
-                                "caption":"normal"
-                            },
-                            {
-                                "id":"disabled",
-                                "caption":"disabled",
-                                "disabled":true
-                            },
-                            {
-                                "id":"image",
-                                "caption":"image",
-                                "imageClass":"xui-icon-xui"
-                            },
-                            {
-                                "type":"split"
-                            },
-                            {
-                                "id":"checkbox 1",
-                                "caption":"checkbox 1",
-                                "type":"checkbox"
-                            },
-                            {
-                                "id":"checkbox 2",
-                                "caption":"checkbox 2",
-                                "type":"checkbox"
-                            }
-                        ],
-                        "caption":"Main Menu"
-                    },
-                    {
-                        "id":"menu2",
-                        "sub":[
-                            {
-                                "id":"sub menu 1",
-                                "caption":"sub menu 1",
-                                "add":"[Ctrl+F]",
-                                "sub":[
-                                    {
-                                        "id":"sub 1",
-                                        "type":"radiobox"
-                                    },
-                                    {
-                                        "id":"sub 2",
-                                        "type":"radiobox"
-                                    },
-                                    {
-                                        "id":"sub 3"
-                                    }
-                                ]
-                            },
-                            {
-                                "id":"sub menu 2",
-                                "caption":"sub menu 2",
-                                "add":"[Ctrl+T]",
-                                "sub":[
-                                    "sub 3",
-                                    "sub 4"
-                                ]
-                            }
-                        ],
-                        "caption":"Writer"
-                    }
-                ])
-                .setTop("44.166666666666664em")
             );
             
             return children;
