1da243bb44ad5f86e05f2ab214b87f1ecff5398a
Bluppie06 <51677733+Bluppie06@users.noreply.github.com>
2019-06-10 21:19:38 +0200

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,45 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.SVGPaper")
+                .setHost(host,"xui_ui_svgpaper1")
+                .setLeft("5.833333333333333em")
+                .setTop("1.6666666666666667em")
+                .setWidth("52.833333333333336em")
+                .setHeight("8.333333333333334em")
+            );
+            
+            host.xui_ui_svgpaper1.append(
+                xui.create("xui.svg.text")
+                .setHost(host,"xui_svg_text1")
+                .setSvgTag("Shapes:Text")
+                .setAttr({
+                    "text":"Welcome to B-Words",
+                    "font-size":"50px",
+                    "font-weight":"bold",
+                    "x":321,
+                    "y":58.5
+                })
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button8")
+                .setDirtyMark(false)
+                .setLeft("13em")
+                .setTop("9.5em")
+                .setWidth("12.083333333333334em")
+                .setHeight("5.416666666666667em")
+                .setCaption("Open B-Words")
+                .setCustomStyle({
+                    "KEY":{
+                        "font-size":"20px"
+                    }
+                })
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
