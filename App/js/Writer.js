657f01e7ecbb6fd2f250722c067d3c205f6f19bf
Bluppie06 <51677733+Bluppie06@users.noreply.github.com>
2019-06-10 21:25:00 +0200

    Updated by CrossUI No-Code App Builder

--- App/js/Writer.js
+++ App/js/Writer.js
@@ -19,7 +19,21 @@ xui.Class('App.Writer', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.RichEditor")
+                .setHost(host,"xui_ui_richeditor2")
+                .setDirtyMark(false)
+                .setLeft("0em")
+                .setTop("0em")
+                .setWidth("67em")
+                .setHeight("50.166666666666664em")
+                .setLabelSize("2em")
+                .setLabelPos("top")
+                .setLabelCaption("B-Words Editor")
+                .setLabelHAlign("left")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
